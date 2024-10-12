7a83e4837c4be7e3b25d634eb61ed66a16a3d26f
Fannynystrom <120643333+Fannynystrom@users.noreply.github.com>
2024-10-12 11:58:39 +0200

    added login/logout in navigation (without function)

--- src/navigate/Navigation.tsx
+++ src/navigate/Navigation.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/NavigationStyles.css'; 
+import '../styles/NavigationStyles.css';
 
 function Navigation() {
   return (
     <nav>
-      <ul>
+      <ul className="nav-links">
         <li>
           <Link to="/">Hem</Link>
         </li>
         <li>
           <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <ul className="auth-links">
+        <li>
+          <Link to="/login">Log out/Login</Link>
         </li>
       </ul>
     </nav>
