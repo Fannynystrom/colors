95acef363fdff6cfa4c84387634a43ecffa58dfc
Fannynystrom <120643333+Fannynystrom@users.noreply.github.com>
2024-10-15 17:17:56 +0200

    a button is visible for admin in store "add product"

--- src/screens/Store.tsx
+++ src/screens/Store.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext'; 
 
 function Store() {
+  const authContext = useContext(AuthContext);
+  const isAdmin = authContext?.role === 'admin'; // kollar om användaren är admin
+
   return (
     <div>
       <h1>Butik</h1>
+      {isAdmin && ( //  knappen visas endast för admin
+        <button>Lägg till produkt</button>
+      )}
     </div>
   );
 }
