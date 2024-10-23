53a2b4491af76f840f8e880ed317c991eef7794a
Fannynystrom <120643333+Fannynystrom@users.noreply.github.com>
2024-10-23 17:57:36 +0200

    inactivity after one minute = logged out and session deleted

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -14,24 +14,52 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const [role, setRole] = useState<string>('');
 
   useEffect(() => {
-    const storedUser = sessionStorage.getItem('user'); 
+    const storedUser = sessionStorage.getItem('user');
     if (storedUser) {
       const user = JSON.parse(storedUser);
       setIsAuthenticated(true);
       setRole(user.role);
     }
+
+    // logga ut användaren efter inaktivitet
+    const handleLogoutDueToInactivity = () => {
+      setIsAuthenticated(false);
+      setRole('');
+      sessionStorage.removeItem('user');
+      window.location.href = '/'; // skickas till login
+    };
+
+    // timer för inaktivitet (1 minut = 60000 ms)
+    let inactivityTimer: NodeJS.Timeout = setTimeout(handleLogoutDueToInactivity, 60000);
+
+    // timern återställs vid aktivitet 
+    const resetTimer = () => {
+      clearTimeout(inactivityTimer);
+      inactivityTimer = setTimeout(handleLogoutDueToInactivity, 60000);
+    };
+
+    // eventlyssnare för att upptäcka aktivitet om man rör mus elr trycker på tangent
+    window.addEventListener('mousemove', resetTimer);
+    window.addEventListener('keypress', resetTimer);
+
+    // städar upp händelser/eventlyssnare
+    return () => {
+      clearTimeout(inactivityTimer);
+      window.removeEventListener('mousemove', resetTimer);
+      window.removeEventListener('keypress', resetTimer);
+    };
   }, []);
 
   const login = (userId: number, userRole: string) => {
     setIsAuthenticated(true);
     setRole(userRole);
-    sessionStorage.setItem('user', JSON.stringify({ userId, role: userRole })); // sparar i sessionStorage
+    sessionStorage.setItem('user', JSON.stringify({ userId, role: userRole }));
   };
 
   const logout = () => {
     setIsAuthenticated(false);
     setRole('');
-    sessionStorage.removeItem('user'); // tar bort användaren från sessionStorage
+    sessionStorage.removeItem('user');
   };
 
   return (
